8e0f972a388b9624b6f7709436c0a482d289d729
LucasPham <ldp43@case.edu>
2020-10-24 10:54:11 -0400

    update 10/24

--- dbconnect.py
+++ dbconnect.py
@@ -3,7 +3,7 @@ import MySQLdb
 def connection():
     conn = MySQLdb.connect(host='localhost',
                             user='root',
-                            passwd='Logan@6824',
+                            passwd='PASSWORD',
                             db='MoneyLine')
     c = conn.cursor()
     return c, conn
