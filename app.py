e6d862cdc00143d0c1d1eada5533e319a53a36d6
agb82 <72327614+agb82@users.noreply.github.com>
2020-11-02 12:38:00 -0500

    Update app.py
    added the function for edit profile

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ import gc, re
 import json
 import requests
 from functools import wraps
+from forms.py import EditProfileForn
 
 import os
 
@@ -171,6 +172,22 @@ def avatar(self, size):
         digest = md5(username.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
 
+@ app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
+
+    
 @ app.route('/newPost', methods=["GET", "POST"])
 @login_required
 def add_post():
